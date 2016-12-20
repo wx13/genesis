9f55c5da0a0512a3dbe555a3eee719d3df6483f1
Jason DeVita <jason@wx13.com>
2016-12-20 11:09:08 -0800

    split flag-processing out of installer.go into own file
    	new file:   installer/flags.go
    	modified:   installer/installer.go

--- installer/installer.go
+++ installer/installer.go
@@ -5,7 +5,6 @@ package installer
 
 import (
 	"archive/zip"
-	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -39,96 +38,6 @@ type Installer struct {
 	Gendir   string
 	DoTags   string
 	SkipTags string
-}
-
-// ParseFlags does all the flag parsing for the installer.
-func (inst *Installer) ParseFlags() {
-
-	// Grab the executable name for usage printout.
-	execName := path.Base(os.Args[0])
-
-	// Main help screen.
-	flag.Usage = func() {
-		fmt.Println("")
-		fmt.Println("Usage:")
-		fmt.Println("")
-		fmt.Printf("  %s -h\n", execName)
-		fmt.Printf("  %s (status|install|remove) [-verbose] [-tmpdir] [-dir] [-tags] [-skip-tags]\n", execName)
-		fmt.Printf("  %s build [dir...]\n", execName)
-		fmt.Printf("  %s rerun\n", execName)
-		fmt.Println("")
-		fmt.Println("Commands:")
-		fmt.Println("")
-		fmt.Println("  status    Show the current installation.")
-		fmt.Println("  install   Run the installer.")
-		fmt.Println("  remove    Reverse the installation process.")
-		fmt.Println("  rerun     Start a command prompt to search/view/edit/run previous commands.")
-		fmt.Println("  build     Add file resources to executable to build a stand-alone installer.")
-		fmt.Println("")
-		fmt.Println("For details on individual command options, run './installer <cmd> -h'.")
-		fmt.Println("")
-		flag.PrintDefaults()
-	}
-
-	// Options for the "run" commands: install, remove, status.
-	runFlag := flag.NewFlagSet("run", flag.ExitOnError)
-	verbose := runFlag.Bool("verbose", false, "Verbose")
-	tmpdir := runFlag.String("tmpdir", "", "Temp directory for unpacked files; empty string == default location")
-	dir := runFlag.String("dir", "~/.genesis", "Storage directory for data. Defaults to ~/.genesis")
-	doTags := runFlag.String("tags", "", "Specify comma-separated tags to run.  Defaults to all.")
-	skipTags := runFlag.String("skip-tags", "", "Specify comma-separated tags to skip.  Defaults to none.")
-	runFlag.Usage = func() {
-		fmt.Println("")
-		fmt.Println("Usage:")
-		fmt.Println("")
-		fmt.Printf("  %s (status|install|remove) [-verbose] [-tmpdir] [-storedir] [-tags] [-skip-tags]\n", execName)
-		fmt.Println("")
-		fmt.Println("Options:")
-		fmt.Println("")
-		runFlag.PrintDefaults()
-		fmt.Println("")
-	}
-
-	buildFlag := flag.NewFlagSet("build", flag.ExitOnError)
-	buildFlag.Usage = func() {
-		fmt.Println("")
-		fmt.Println("Usage:")
-		fmt.Println("")
-		fmt.Printf("  %s build [list of directories]\n", execName)
-		fmt.Println("")
-	}
-	rerunFlag := flag.NewFlagSet("rerun", flag.ExitOnError)
-
-	// Print help screen if no arguments are given.
-	if len(os.Args) <= 1 {
-		flag.Usage()
-		os.Exit(1)
-	}
-
-	// Get the subcommand.
-	cmd := os.Args[1]
-
-	// Parse the subcommand options.
-	switch cmd {
-	case "install", "remove", "status":
-		runFlag.Parse(os.Args[2:])
-	case "build":
-		buildFlag.Parse(os.Args[2:])
-	case "rerun":
-		rerunFlag.Parse(os.Args[2:])
-	default:
-		flag.Usage()
-		os.Exit(1)
-	}
-
-	inst.Cmd = cmd
-	inst.Verbose = *verbose
-	inst.DoTags = *doTags
-	inst.SkipTags = *skipTags
-
-	inst.Tmpdir, _ = ioutil.TempDir(*tmpdir, "genesis")
-	inst.Dir = genesis.ExpandHome(*dir)
-
 }
 
 // New creates a new installer object.
